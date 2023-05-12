107a0b40796f74cba31efd60599009117b8b91b1
1a35e1 <1a35e1@protonmail.com>
2023-05-13 00:36:55 +0100

    feat: Prove that message was signed 3/3

--- packages/signaturi/__tests__/hello.spec.ts
+++ packages/signaturi/__tests__/hello.spec.ts
@@ -1,11 +1,29 @@
 import { hello } from "../src/hello"
+import { Wallet, ethers } from 'ethers'
 
-describe('hello', () => {
+const DATA = "Hello, world!"
 
-    it('lasdlsadasd', async () => {
-        const resp = hello()
-        expect(resp).toBe('Worldasds')
+describe('how signing works', () => {
+
+    it('message ', async () => {
+        const alice = Wallet.createRandom()
+        const bob = Wallet.createRandom()
+        const charlie = Wallet.createRandom()
+
+        const messageHash = ethers.utils.hashMessage(DATA);
+
+        // Recover the addresses from the signatures
+        const recoveredAddress1 = ethers.utils.recoverAddress(messageHash, await alice.signMessage(DATA));
+        const recoveredAddress2 = ethers.utils.recoverAddress(messageHash, await bob.signMessage(DATA));
+        const recoveredAddress3 = ethers.utils.recoverAddress(messageHash, await charlie.signMessage(DATA));
+
+        // Check the recovered address against the expected address
+        const isValidSignature = (recoveredAddress1.toLowerCase() === alice.address.toLowerCase() &&
+            recoveredAddress2.toLowerCase() === bob.address.toLowerCase() &&
+            recoveredAddress3.toLowerCase() === charlie.address.toLowerCase()
+        ) ? true : false
+
+        expect(isValidSignature).toBe(true)
     })
-
 
 })
