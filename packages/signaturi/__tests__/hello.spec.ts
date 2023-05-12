d2495f844d70c5dbdd82e28f22d735ed8080725e
1a35e1 <1a35e1@protonmail.com>
2023-05-12 23:09:23 +0100

    feat: Create package; Configure jest

---
+++ packages/signaturi/__tests__/hello.spec.ts
@@ -0,0 +1,11 @@
+import { hello } from "../src/hello"
+
+describe('hello', () => {
+
+    it('lasdlsadasd', async () => {
+        const resp = hello()
+        expect(resp).toBe('Worldasds')
+    })
+
+
+})
