3f51c65bebe441ad39ff0521411cdda223e0103f
Mihail <lepadatu.mihail1@gmail.com>
2023-05-13 11:12:43 +0100

    feat: Initialise feed page + font styling

--- packages/nextjs/pages/_app.tsx
+++ packages/nextjs/pages/_app.tsx
@@ -39,8 +39,61 @@ const ScaffoldEthApp = ({ Component, pageProps }: AppProps) => {
         theme={{
           // Override any other properties from default theme
           fontFamily:
-            " -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji",
-          spacing: { xs: "1rem", sm: "1.2rem", md: "1.8rem", lg: "2.2rem", xl: "2.8rem" },
+            "Bariol, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji",
+
+          shadows: {
+            xs: "0 0.0625rem 0.1875rem rgba(0, 0, 0, 0.05), 0 0.0625rem 0.125rem rgba(0, 0, 0, 0.1)",
+            sm: "0 0.10125rem 0.30375rem rgba(0, 0, 0, 0.05), 0 0.10125rem 0.2025rem rgba(0, 0, 0, 0.1)",
+            md: "0 0.1638rem 0.4917rem rgba(0, 0, 0, 0.05), 0 0.1638rem 0.3276rem rgba(0, 0, 0, 0.1)",
+            lg: "0 0.2651rem 0.7953rem rgba(0, 0, 0, 0.05), 0 0.2651rem 0.5302rem rgba(0, 0, 0, 0.1)",
+            xl: "0 0.4290rem 1.2860rem rgba(0, 0, 0, 0.05), 0 0.4290rem 0.8580rem rgba(0, 0, 0, 0.1)",
+          },
+
+          fontSizes: {
+            xs: "0.75rem" /* 0.75 * 1.618^0 = 0.75rem */,
+            sm: "1.2135rem" /* 0.75 * 1.618^1 ≈ 1.2135rem */,
+            md: "1.96282rem" /* 0.75 * 1.618^2 ≈ 1.96282rem */,
+            lg: "3.17649rem" /* 0.75 * 1.618^3 ≈ 3.17649rem */,
+            xl: "5.13931rem" /* 0.75 * 1.618^4 ≈ 5.13931rem */,
+          },
+
+          radius: {
+            xs: "0.125rem" /* 0.125 * 1.618^0 = 0.125rem */,
+            sm: "0.20225rem" /* 0.125 * 1.618^1 ≈ 0.20225rem */,
+            md: "0.32718rem" /* 0.125 * 1.618^2 ≈ 0.32718rem */,
+            lg: "0.52933rem" /* 0.125 * 1.618^3 ≈ 0.52933rem */,
+            xl: "2rem" /* 0.125 * 1.618^4 ≈ 0.85651rem */,
+          },
+
+          spacing: {
+            xs: "0.625rem" /* 0.625 * 1.618^0 = 0.625rem */,
+            sm: "1.01125rem" /* 0.625 * 1.618^1 ≈ 1.01125rem */,
+            md: "1.63504rem" /* 0.625 * 1.618^2 ≈ 1.63504rem */,
+            lg: "2.64616rem" /* 0.625 * 1.618^3 ≈ 2.64616rem */,
+            xl: "4.28194rem" /* 0.625 * 1.618^4 ≈ 4.28194rem */,
+            xxl: "6.93062rem" /* 0.625 * 1.618^5 ≈ 4.28194rem */,
+          },
+
+          /* Golden ratio breakpoints */
+          breakpoints: {
+            xs: "30em" /* 30 * 1.618^0 = 30em */,
+            sm: "48.54em" /* 30 * 1.618^1 ≈ 48.54em */,
+            md: "78.49em" /* 30 * 1.618^2 ≈ 78.49em */,
+            lg: "126.83em" /* 30 * 1.618^3 ≈ 126.83em */,
+            xl: "205.17em" /* 30 * 1.618^4 ≈ 205.17em */,
+          },
+
+          headings: {
+            fontWeight: 700,
+            sizes: {
+              h1: { fontSize: "9.68310rem", lineHeight: 1.3, fontWeight: undefined },
+              h2: { fontSize: "5.98297rem", lineHeight: 1.35, fontWeight: undefined },
+              h3: { fontSize: "3.70013rem", lineHeight: 1.4, fontWeight: undefined },
+              h4: { fontSize: "2.28872rem", lineHeight: 1.45, fontWeight: undefined },
+              h5: { fontSize: "1.41525rem", lineHeight: 1.5, fontWeight: undefined },
+              h6: { fontSize: "0.875rem", lineHeight: 1.5, fontWeight: undefined },
+            },
+          },
         }}
       >
         <NextNProgress />
@@ -51,7 +104,7 @@ const ScaffoldEthApp = ({ Component, pageProps }: AppProps) => {
         >
           <div className="flex flex-col min-h-screen">
             <Header />
-            <main className="relative flex flex-col flex-1">
+            <main className="relative flex flex-col flex-1 bg-white">
               <Component {...pageProps} />
             </main>
             <Footer />
