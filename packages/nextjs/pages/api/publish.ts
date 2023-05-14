96f7ebbc04268584a2abceecb6f70df3a3d04edd
1a35e1 <1a35e1@protonmail.com>
2023-05-14 02:41:08 +0100

    feat: Set feed.published_at

---
+++ packages/nextjs/pages/api/publish.ts
@@ -0,0 +1,26 @@
+import { db } from "@vercel/postgres";
+
+export default async function handler(req, res) {
+  const payload = req.body;
+  const client = await db.connect();
+
+  // --- fetch the record
+  const resp = await client.query(`SELECT * FROM feed WHERE title = $1 LIMIT 1`, [payload.id]);
+  const feedItem = resp.rows[0];
+
+  try {
+    // --- update the record
+    await client.query(
+      `
+      UPDATE feed
+      SET
+         published_at = now()
+      WHERE
+        title = $1 AND published_at IS NULL`,
+      [payload.id],
+    );
+    return res.status(200).json({ record: feedItem });
+  } catch (err) {
+    console.log(err);
+  }
+}
